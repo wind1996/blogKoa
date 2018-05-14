7553a5a9673bf2a715185d1e6d3cd1acfd4cf0e8
Good__luck <2924456233@qq.com>
2018-05-14 12:29:33 +0800

    增加lab模型

---
+++ models/lab.js
@@ -0,0 +1,35 @@
+const db = require('../middleware/db');
+//实验室
+module.exports = db.defineModel('lab', {
+    id: {
+        type: db.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    title: db.STRING(100),
+    description: {
+        type: db.STRING(200),
+        allowNull: true
+    },
+    url: {
+        type: db.STRING(100),
+        allowNull: true
+    },
+    bg_url: {
+        type: db.STRING(100),
+        allowNull: true
+    },
+    location: {
+        type: db.STRING(100),
+        allowNull: true
+    },
+    online: {
+        type: db.BOOLEAN,
+    },
+    click_count: {
+        type: db.INTEGER,
+    },
+    like_count: {
+        type: db.INTEGER,
+    }
+});
