b0efb5ebe511396be3582bea03e798c85c181f30
kongdepeng-lhq <2924456233@qq.com>
2018-05-18 11:12:30 +0800

    对开发和线上环境做区分，增加dev_config.js和prod_config.js

---
+++ config/prod_config.js
@@ -0,0 +1,12 @@
+/**
+ * 线上环境的配置
+ * @type {{dialect: string, database: string, username: string, password: string, host: string, port: number}}
+ */
+module.exports = {
+    dialect: 'mysql',
+    database: 'blog',
+    username: 'root',
+    password: '',
+    host: 'localhost',
+    port: 3308
+};
