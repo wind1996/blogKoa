a88e2f30940273e0ae61c2336f287525949ed3bd
Good__luck <2924456233@qq.com>
2018-05-29 18:09:40 +0800

    去除不必要路由

--- controller/article/reprintController.js
+++ controller/article/reprintController.js
@@ -1,60 +1,10 @@
-const {recommendAdapter,recommendStatisticsAdapter} = require('../../common/Adapter');
+const {recommendAdapter, recommendStatisticsAdapter} = require('../../common/Adapter');
 const recommendServer = require('../../service/recommendServer');
 const pageConfig = require('../../config/config').article;
 
 var data = {
     title: '博客-转载',
     reprintList: [
-        {
-            title: '这是标题',
-            list: [
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-            ]
-        },
-        {
-            title: '这是标题',
-            list: [
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-            ]
-        },
-        {
-            title: '这是标题',
-            list: [
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-            ]
-        },
-        {
-            title: '这是标题',
-            list: [
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-            ]
-        },
-        {
-            title: '这是标题',
-            list: [
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-                {href: 'www.qq.com', name: '第一条信息链接，第一条信息链接'},
-            ]
-        },
         {
             title: '这是标题',
             list: [
@@ -72,58 +22,22 @@ var data = {
             from: '掘金',
             detail: 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ',
             href: 'www.baidu.com'
-        },
-        {
-            title: '一周精通VUE JS',
-            from: '掘金',
-            detail: 'loremm lorem orem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ',
-            href: 'www.baidu.com'
-        },
-        {
-            title: '一周精通VUE JS',
-            from: '掘金',
-            detail: 'lorem  lorem lorem loreem lorem lorem lorem lorem ',
-            href: 'www.baidu.com'
-        },
-        {
-            title: '一周精通VUE JS',
-            from: '掘金',
-            detail: 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ',
-            href: 'www.baidu.com'
-        },
-        {
-            title: '一周精通VUE JS',
-            from: '掘金',
-            detail: '我肯定超了lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ',
-            href: 'www.baidu.com'
-        },
-        {
-            title: '一周精通VUE JS',
-            from: '掘金',
-            detail: 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ',
-            href: 'www.baidu.com'
         }
-    ],
-    pagination: {
-        size: 10,
-        sumPage: 100,
-        currentPage: 1,
-        baseUrl: '/reprint?page='
-    }
+    ]
 };
 
 const reprintController = {
-    async render(ctx, next) {
-        const type = ctx.query.type ? ctx.query.type.split(',') : [];
-        console.log(type.length === 0);
-        if (type.length === 0) {
-            await ctx.render('page/reprint/reprint', data);
-        } else if (type.includes('follow')) {
-            await ctx.render('page/reprint/reprint-follow', data);
-        } else {
-            await ctx.render('page/reprint/reprint-recommended', data);
-        }
-    },
+    /* async render(ctx, next) {
+         const type = ctx.query.type ? ctx.query.type.split(',') : [];
+         console.log(type.length === 0);
+         if (type.length === 0) {
+             await ctx.render('page/reprint/reprint', data);
+         } else if (type.includes('follow')) {
+             await ctx.render('page/reprint/reprint-follow', data);
+         } else {
+             await ctx.render('page/reprint/reprint-recommended', data);
+         }
+     },*/
     async renderStatistics(ctx, next) {
         const page = Number(ctx.query.page) || 1;
         const size = Number(ctx.query.size) || pageConfig.statisticsSize;
